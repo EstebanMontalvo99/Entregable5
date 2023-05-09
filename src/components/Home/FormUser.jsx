fe27fd6fb91dd183c5e85af711c1b3c5a214dabb
EstebanMontalvo99 <estebanmontalvo99@hotmail.com>
2023-05-08 23:50:22 -0500

    home styles

--- src/components/Home/FormUser.jsx
+++ src/components/Home/FormUser.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { setTrainerName } from '../../store/slices/trainerName.slice'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setTrainerName } from '../../store/slices/trainerName.slice';
+import { useNavigate } from 'react-router-dom';
 
 const FormUser = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     dispatch(setTrainerName(e.target.firstChild.value.trim()));
-    navigate("/pokedex")
-  }
+    navigate("/pokedex");
+  };
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" />
-      <button>Start</button>
+    <form className='home__form' onSubmit={handleSubmit}>
+      <input className='home__input' placeholder='Your name' type="text" />
+      <button className='home__btn'>Start</button>
     </form>
-  )
-}
+  );
+};
 
-export default FormUser
+export default FormUser;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import FormUser from '../components/Home/FormUser'
-
+import React from 'react';
+import FormUser from '../components/Home/FormUser';
+import styles from "../components/Home/styles/home.css";
 const Home = () => {
   return (
-    <div>
-      <h1>Pokedex</h1>
-      <h2>Hi Trainer</h2>
-      <p>Please enter your name to start</p>
+    <div className='home'>
+      <img className='home__title' src="/image.png" alt="" />
+      <h2 className='home__greetings'>Hi Trainer</h2>
+      <p className='home__text'>Please enter your name to start</p>
       <FormUser />
+      <footer className='home__footer'>
+        <div className="red__bar"></div>
+        <div className="black__bar">
+          <div className="circle1">
+            <div className="circle2"></div>
+          </div>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
